705025793391ae8413c8b5148c656b132d754fb1
_hasansharaf <hasansharaf456@gmail.com>
2023-05-19 14:44:23 +0300

    fix error CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
+        // \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -44,6 +44,8 @@ class Kernel extends HttpKernel
             'throttle:api',
            // \App\Http\Middleware\IntegraaAuth::class, //FIXME: Remove the comment to enable auth
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Fruitcake\Cors\HandleCors::class,
+
         ],
        
         
