d75e0865a1f13d2f24aa1cd1b00442eb8ba7c347
HasanSharaf <hasansharaf456@gmail.com>
2023-08-20 22:16:45 +0300

    fix/fix login into admin

--- Modules/Admin/Entities/Admin.php
+++ Modules/Admin/Entities/Admin.php
@@ -5,10 +5,13 @@ namespace Modules\Admin\Entities;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Modules\User\Entities\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    
     protected $table = 'admins';
     protected $fillable = [
         'id',
@@ -21,6 +24,25 @@ class Admin extends Model
         'birthday',
         'admin_picture',
         'type',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
     
 }
